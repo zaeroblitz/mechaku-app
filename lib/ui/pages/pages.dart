b944953abb8af533d9bee38984665b081513e8de
Zaero Blitz <zaeroblitz@gmail.com>
2021-08-09 11:57:40 +0700

    Fix user wishlists

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:mechaku/models/models.dart';
 import 'package:mechaku/services/services.dart';
 import 'package:mechaku/theme.dart';
--- lib/ui/pages/wishlist/WishlistPage.dart
+++ lib/ui/pages/wishlist/WishlistPage.dart
@@ -6,34 +6,64 @@ class WishlistPage extends StatelessWidget {
     PageProvider pageProvider = Provider.of<PageProvider>(context);
     AuthProvider authProvider = Provider.of<AuthProvider>(context);
     UserModel user = authProvider.user;
-    print(user.wishlist.map((e) => e.toString()));
+    user.wishlist.forEach((element) {
+      print(element);
+    });
 
     FirebaseFirestore firestore = FirebaseFirestore.instance;
     CollectionReference products = firestore.collection('products');
 
-    getWishlistData() {
-      for (var item in user.wishlist) {
-        return products.where('id', isEqualTo: item).snapshots();
-      }
-    }
+    // getWishlistData() {
+    //   for (var item in user.wishlist) {
+    //     return products.where('id', isEqualTo: item).snapshots();
+    //   }
+    // }
 
     Widget wishlistItems() {
-      return StreamBuilder<QuerySnapshot>(
-          stream: getWishlistData(),
-          builder: (context, snapshot) {
-            return SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Container(
-                margin: EdgeInsets.all(30),
-                child: Column(
-                  children: snapshot.data.docs
-                      .map((doc) => WishlistItem(doc.get('gallery')[0],
-                          doc.get('name'), doc.get('price')))
-                      .toList(),
-                ),
-              ),
-            );
-          });
+      // return StreamBuilder<QuerySnapshot>(
+      //     stream: getWishlistData(),
+      //     builder: (context, snapshot) {
+      //       return SingleChildScrollView(
+      //         scrollDirection: Axis.vertical,
+      //         child: Container(
+      //           margin: EdgeInsets.all(30),
+      //           child: Column(
+      //             children: snapshot.data.docs
+      //                 .map((doc) => WishlistItem(doc.get('gallery')[0],
+      //                     doc.get('name'), doc.get('price')))
+      //                 .toList(),
+      //           ),
+      //         ),
+      //       );
+      //     });
+
+      return SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          margin: EdgeInsets.all(30),
+          child: Column(
+            children: user.wishlist
+                .map((e) => FutureBuilder<QuerySnapshot>(
+                    future: products.where('id', isEqualTo: e).get(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          children: snapshot.data.docs
+                              .map((doc) => WishlistItem(doc.get('gallery')[0],
+                                  doc.get('name'), doc.get('price')))
+                              .toList(),
+                        );
+                      } else {
+                        return SpinKitWave(
+                          size: 20,
+                          color: iconColor,
+                        );
+                      }
+                    }))
+                .toList(),
+          ),
+        ),
+      );
     }
 
     Widget emptyState() {
